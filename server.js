195e382f5655e1c42a5e2ba7fce51406889bdafa
TheIronDeveloper <theirondeveloper@gmail.com>
2013-11-02 14:15:54 -0500

    Added a basic session. Will continue later..

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ var express = require('express'),
 	app = express();
 
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.use(express.cookieParser());
+app.use(express.session({secret: '1234567890QWERTY'}));
 
-app.get('/', function(req, res){
-  res.send('This is a node generated homepage');
+app.get('/', function(req, res){	
+	res.send('Your last page was '+req.session.prevPage+'<br/>');	
+});
+
+app.get('/test1', function(req, res){
+	res.send('test1, your session should reflect you visited test1 last');
+	req.session.prevPage = 'test1';
+});
+app.get('/test2', function(req, res){
+	res.send('test1, your session should reflect you visited test2 last');
+	req.session.prevPage = 'test2';
+});
+
+app.listen(3000, function(){
+	console.log('Listening on port 3000');
 });
