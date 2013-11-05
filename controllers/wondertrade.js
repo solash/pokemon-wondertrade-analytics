fdb78a8b22739766e0cc59ec67b70695e97ed02a
TheIronDeveloper <theirondeveloper@gmail.com>
2013-11-04 21:45:02 -0600

    Cleaned up the display, added a sanitize data step, added a 'generateTestCases' call (to get started faster)

--- controllers/wondertrade.js
+++ controllers/wondertrade.js
@@ -42,16 +42,35 @@ exports.initController =  function(app, dataStore, util) {
 		var WondertradeParams = request.body,
 			wondertrade = WondertradeModel(WondertradeParams),
 			serializedWondertrade = JSON.stringify(wondertrade);
+		if(wondertrade) {
+			dataStore.lpush('wondertrade', serializedWondertrade, function(err, size) {			
+				response.render('wondertrade/new', {
+					title: 'New Wonder Trade',
+					pokemonList: PokemonList,
+					state:	'post'
+				});
+				console.log('A new wondertrade was added');
+			});	
+		} else {
+			response.send('There was an error with the pokemon you were loading...');
+		}
+				
+	});
 
-		dataStore.lpush('wondertrade', serializedWondertrade, function(err, size) {			
-			response.render('wondertrade/new', {
-				title: 'New Wonder Trade',
-				pokemonList: PokemonList,
-				state:	'post'
-			});
-			console.log('A new wondertrade was added');
-		});
+	// For development purposes only. Discard Afterward. Like 4srs.
+	app.get('/wondertrade/generateTestData', function(request, response){
+		var test1 = WondertradeModel({"pokemonId":"10","pokemonNickname":"10","hasItem":true,"hasHiddenAbility":false,"isShiny":false,"gender":"male","trainerCountry":"USA","trainerCountrySub1":"Test1","date":"2013-11-04","userId":"anonymous"}),
+			test2 = WondertradeModel({"pokemonId":"25","pokemonNickname":"","hasItem":false,"hasHiddenAbility":false,"isShiny":false,"gender":"male","trainerCountry":"Japan","trainerCountrySub1":"Test2","date":"2013-11-04","userId":"Test"}),
+			test3 = WondertradeModel({"pokemonId":"35","pokemonNickname":"35","hasItem":true,"hasHiddenAbility":true,"isShiny":false,"gender":"female","trainerCountry":"France","trainerCountrySub1":"Test3","date":"2013-11-05","userId":"anonymous"}),
+			test4 = WondertradeModel({"pokemonId":"100","pokemonNickname":"","hasItem":true,"hasHiddenAbility":false,"isShiny":false,"gender":"female","trainerCountry":"France","trainerCountrySub1":"","date":"2013-11-04","userId":"anonymous"}),
+			test5 = WondertradeModel({"pokemonId":"150","pokemonNickname":"Awesome","hasItem":false,"hasHiddenAbility":false,"isShiny":true,"gender":"male","trainerCountry":"France","trainerCountrySub1":"Test5","date":"2013-11-04","userId":"anonymous"}),
+			test6 = WondertradeModel({"pokemonId":"704","pokemonNickname":"Our Lord","hasItem":true,"hasHiddenAbility":true,"isShiny":true,"gender":"male","trainerCountry":"USA","trainerCountrySub1":"","date":"2013-11-04","userId":"Test"}),
+			testArray = [test1, test2, test3, test4, test5, test6];
 
-		
+		for(var testWT in testArray) {
+			var stringifiedWT = JSON.stringify(testArray[testWT]);
+			dataStore.lpush('wondertrade', stringifiedWT);
+		}
+		response.send('Alright, the test data has been loaded!');
 	});
 };
--- models/wondertrade.js
+++ models/wondertrade.js
@@ -1,17 +1,67 @@
+function sanitizeParams(params) {
+	var currentDate;
+
+	// Sanitize Date
+	if(params.date) {
+		currentDate = params.date;
+	} else {
+		var now = new Date(),
+			year = now.getFullYear(),
+			month = ('0'+(now.getMonth()+1)).slice(-2),
+			day = ('0'+(now.getDate())).slice(-2);
+		currentDate = [year, month, day].join('-');
+	}
+
+	// Sanitize Gender values
+	if(params.gender === "male" || params.gender === "female") {
+		params.trainerGender = params.gender;
+	} else {
+		params.trainerGender = "";
+	}
+
+	// Sanitize pokemonId
+	var pokemonId = parseInt(params.pokemonId, 10);
+	if(pokemonId >=1 && pokemonId <=718) {
+		pokemonId = pokemonId;
+	} else {
+		pokemonId = false;
+	}
+
+	params.pokemonId = pokemonId;
+	params.pokemonNickname = params.pokemonNickname || '';
+	params.hasItem = (params.hasItem ? true : false);
+	params.hasHiddenAbility = (params.hasHiddenAbility ? true : false);
+	params.isShiny = (params.isShiny ? true : false);
+	params.trainerGender = params.trainerGender || '';
+	params.trainerCountry = params.trainerCountry || '';
+	params.trainerCountrySub1 = params.trainerCountrySub1  || '';
+	params.date = currentDate;
+	params.userId = params.userId || 'anonymous';
+
+	return params;
+}
+
+
 exports.model = function(params) {
-	var pokemonModel = {};
+	
+	params = sanitizeParams(params);
 
-	pokemonModel.pokemonId = params.pokemonId || 0;
-	pokemonModel.pokemonNickname = params.pokemonNickname || '';
-	pokemonModel.hasItem = params.hasItem || '';
-	pokemonModel.hasHiddenAbility = params.hasHiddenAbility || '';
-	pokemonModel.isShiny = params.isShiny || '';
-	pokemonModel.item = params.item || '';	
-	pokemonModel.trainerGender = params.trainerGender || '';
-	pokemonModel.trainerCountry = params.trainerCountry || '';
-	pokemonModel.trainerCountrySub1 = params.trainerCountrySub1  || '';
-	pokemonModel.date = params.date || new Date();
-	pokemonModel.userId = params.userId || 123123123;
+	var pokemonModel = {
+		"pokemonId" : params.pokemonId,
+		"pokemonNickname" : params.pokemonNickname,
+		"hasItem" : params.hasItem,
+		"hasHiddenAbility" : params.hasHiddenAbility,
+		"isShiny" : params.isShiny,
+		"trainerGender" : params.trainerGender,
+		"trainerCountry" : params.trainerCountry,
+		"trainerCountrySub1" : params.trainerCountrySub1,
+		"date" : params.date,
+		"userId" : params.userId
+	};
 
-	return pokemonModel;
+	if(!pokemonModel.pokemonId) {
+		return false;
+	} else {
+		return pokemonModel;
+	}
 };
