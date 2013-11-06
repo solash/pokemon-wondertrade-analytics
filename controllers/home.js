03302946589fea5c1fdd39f05df737d8a9c858ea
TheIronDeveloper <theirondeveloper@gmail.com>
2013-11-05 19:05:39 -0600

    Added about page, cleaned up the links on the homepage.

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,6 @@
 exports.initController = function(app, dataStore) {
 	app.get('/', function(request, response){	
 		response.render('home', {
-			date: new Date(),
 			title: 'Homepage Title'
 		});
 		dataStore.get('test', function(err, result) {
@@ -10,5 +9,11 @@ exports.initController = function(app, dataStore) {
 		dataStore.lrange('wondertrade' ,0, -1, function(error, result){
 			console.log(result);
 		});
+	});
+
+	app.get('/about', function(request, response){	
+		response.render('about', {
+			title: 'About this Project'
+		});		
 	});
 };
