24289548154bba2e2f4e929e2ce203d218b79382
Tyler Stark <theirondeveloper@gmail.com>
2013-11-16 13:26:55 -0600

    Adding heroku redis config

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ var express = require('express'),
 	ejs = require('ejs'),
 	engine = require('ejs-locals'),
 	redis = require('redis'),
-	dataStore = redis.createClient(),
+	dataStore;,
 	app = express();
+
+if (process.env.REDISTOGO_URL) {
+	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+	dataStore = require("redis").createClient(rtg.port, rtg.hostname);
+	redis.auth(rtg.auth.split(":")[1]);
+} else {
+	dataStore = require("redis").createClient();
+}
 
 app.engine('ejs', engine);
 app.use(express.bodyParser());
