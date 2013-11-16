b6dcc1fbc85fb180294c842282ef20923f801902
Tyler Stark <theirondeveloper@gmail.com>
2013-11-16 16:39:40 -0600

    So... maybe the port I am pointing to can be the issue...

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ Date.prototype.customFormatDate = function(){
 // 	dataStore.set('test', newValue);
 // });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 5000, function(){
 	console.log('Listening on port 3000..');
 });
