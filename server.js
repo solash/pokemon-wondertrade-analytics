bab4bb15e5f04b33af1e3a204bd0bb16a8b99900
Tyler Stark <theirondeveloper@gmail.com>
2013-11-16 15:46:29 -0600

    So... I typoed a lil bit. Hope this fixes the problem.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var express = require('express'),
 if (process.env.REDISTOGO_URL) {
 	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
 	dataStore = require("redis").createClient(rtg.port, rtg.hostname);
-	redis.auth(rtg.auth.split(":")[1]);
+	dataStore.auth(rtg.auth.split(":")[1]);
 } else {
 	dataStore = require("redis").createClient();
 }
