78d9a42cfc98ce1ecac0908397ebde7dcf18b08e
Tyler Stark <theirondeveloper@gmail.com>
2013-12-01 14:50:53 -0600

    Added the reddit username portion to the site.

--- controllers/home.js
+++ controllers/home.js
@@ -46,38 +46,47 @@ exports.initController = function(app, dataStore) {
 		});		
 	});
 
-	app.get('/contributers', function(request, response){	
+	app.get('/contributers', function(request, response){
 
-		dataStore.lrange('userTable' , 0, -1, function(error, result){			
+        dataStore.lrange('redditUser' , 0, -1, function(error, result){
+            var redditUsers = {};
+            for(var user in result) {
+                var parsedUser = JSON.parse(result[user]);
+                redditUsers[parsedUser.userId] = {redditUserName: parsedUser.redditUserName};
+            }
 
-			var userTable = {};
-			for(var user in result) {
-				var parsedUser = JSON.parse(result[user]);
-				userTable[parsedUser.id] = {username: parsedUser.username, count: 0, id: parsedUser.id};
-			}
+            dataStore.lrange('userTable' , 0, -1, function(error, result){
 
-			dataStore.lrange('wondertrade' , 0, -1, function(error, result){
-				var highChartsData = new HighChartsData(result),
-					userTableWithCounts = highChartsData.getCountsByUserIdAndUserTable(false, userTable),
-					trendingPokemonChart = highChartsData.getCountTrendsByUsers(false, userTable);
+                var userTable = {};
+                for(var user in result) {
+                    var parsedUser = JSON.parse(result[user]);
+                    userTable[parsedUser.id] = {username: parsedUser.username, count: 0, id: parsedUser.id};
+                }
 
-				
-				trendingPokemonChart = _.sortBy(trendingPokemonChart, 'fullCount');
-				trendingPokemonChart.reverse();
-				trendingPokemonChart = _.first(trendingPokemonChart, 10);
-			
-				response.render('contributers', {
-					title: 'Project Contributers',
-					pageState: '',
-					user: request.user,
-					trendingPokemonChart: JSON.stringify(trendingPokemonChart),
-					userTable: userTableWithCounts
-				});
+                dataStore.lrange('wondertrade' , 0, -1, function(error, result){
+                    var highChartsData = new HighChartsData(result),
+                        userTableWithCounts = highChartsData.getCountsByUserIdAndUserTable(false, userTable),
+                        trendingPokemonChart = highChartsData.getCountTrendsByUsers(false, userTable);
 
-			});
 
-			
-		});		
+                    trendingPokemonChart = _.sortBy(trendingPokemonChart, 'fullCount');
+                    trendingPokemonChart.reverse();
+                    trendingPokemonChart = _.first(trendingPokemonChart, 10);
+
+                    response.render('contributers', {
+                        title: 'Project Contributers',
+                        pageState: '',
+                        user: request.user,
+                        trendingPokemonChart: JSON.stringify(trendingPokemonChart),
+                        userTable: userTableWithCounts,
+                        redditUsers: redditUsers
+                    });
+
+                });
+
+            });
+
+        });
 	});
 
 	app.get('/help', function(request, response){		
