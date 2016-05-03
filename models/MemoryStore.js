b5caa2e9deaa3dcafeb8586ae779791634d72707
Tyler Stark <theirondeveloper@gmail.com>
2016-05-02 22:55:52 -0700

    Fetch Redis in smaller chunks.
    So I had a hunch, that the slowest part of the app was the app was
    fetching data from redis. It turns out that the redis client's
    lrange function is blocking, and we were fetching a large amount
    of data.
    
    I wanted to unblock the thread, so I changed the `lrange 0, -1` into
    small chunks, `lrange 0 1000`, `lrange 1000 2000`, ...
    
    I was expecting to at the very least unblock GET/POST requests.
    I was not expecting a 55x speed improvement. Fetching everything
    and concatting it to an array dramatically improves performance.

--- models/MemoryStore.js
+++ models/MemoryStore.js
@@ -17,6 +17,30 @@ for(var country in CountryList) {
 	CountryHash[CountryList[country].id] = CountryList[country].name;
 }
 
+/**
+ * So here's the thing. Under the covers, fetching data from redis is blocking.
+ * Rather than trying to fetch everything from redis in one fell swoop, which
+ * ends up blocking GET / POST requests, I'm chunking it out smaller fetches.
+ *
+ * As a quick comparision with 125,226:
+ * Getting everything at once: 52099.416ms (blocking)
+ * Getting everything in chunks: 935.059ms (non-blocking)
+ *
+ * @param callback {Function} - return the collective sum of redis enteries to the highcharts function
+ */
+function getWonderTrades(index, max, step, result, redisStore, callback) {
+
+	if (index >= max) {
+		return callback(result);
+	}
+
+	redisStore.lrange('wondertrade' , index, index + step, function(error, chunk) {
+		setTimeout(function() {
+			getWonderTrades(index + step, max, step, result.concat(chunk), redisStore, callback);
+		}, 1);
+	});
+}
+
 var MemoryStore = function(redisStore) {
 	this.redisStore = redisStore;
 	this.store = {
@@ -31,12 +55,22 @@ var MemoryStore = function(redisStore) {
 
 // Refresh HighCharts Data from the RedisServer
 MemoryStore.prototype.refreshHighCharts = function() {
-	var self = this;
 
-	if(this.redisStore) {
-		this.redisStore.lrange('wondertrade' ,0, -1, function(error, result){
-			self.store.highChartsData.refreshData(result);
-			self.store.highChartsSize = result.length;
+	var self = this;
+	var highChartsSize;
+	var redisStore = this.redisStore;
+	var store = self.store;
+
+	if(redisStore) {
+
+		redisStore.llen('wondertrade', function(err, size) {
+			highChartsSize = size;
+			store.highChartsSize = size;
+
+			// Fetch all the data in chunks
+			getWonderTrades(0, highChartsSize, 1000, [], redisStore, function(result) {
+				store.highChartsData.refreshData(result);
+			});
 		});
 	}
 };
