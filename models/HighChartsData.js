49e438cf05d3c6509c49a28bf4c56fa43d5d38af
solash <33701238+solash@users.noreply.github.com>
2017-11-15 20:05:35 +0000

    Updating Pokémon count to include Marshadow

--- models/HighChartsData.js
+++ models/HighChartsData.js
@@ -1,7 +1,7 @@
 var async = require('async'),
 	customFormatDate = require('../lib/customFormatDate'),
 	pokeballTypes = require('../data/pokeballTypes'),
-	TOTAL_POKEMON_COUNT = 801;
+	TOTAL_POKEMON_COUNT = 802;
 
 function formatDateFromString(dateString){
 	var formattedDate = dateString.split('-'),
