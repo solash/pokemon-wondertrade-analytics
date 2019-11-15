830a52abea1d0c7578b88b01bb0daa6f2c17b75a
solash <33701238+solash@users.noreply.github.com>
2019-11-15 23:23:42 +0000

    Sword/Shield update

--- controllers/data.js
+++ controllers/data.js
@@ -74,7 +74,7 @@ module.exports = function(app, dataStore, MemoryStore) {
 		var pokemonId = parseInt(req.params.pokemonId, 10),
 			pokemonName = PokemonHash[pokemonId];
 
-		if (!(pokemonId > 0 && pokemonId <= 807)) {
+		if (!(pokemonId > 0 && pokemonId <= 890)) {
 			return res.redirect('404');
 		}
 
